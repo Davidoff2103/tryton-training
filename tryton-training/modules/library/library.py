83c80f726d3d372e2500aab02bceb2a035a54f92
Davidoff2103 <davidoff.d777@gmail.com>
2021-06-09 11:12:59 +0200

    Segon exercici

--- tryton-training/modules/library/library.py
+++ tryton-training/modules/library/library.py
@@ -1,12 +1,13 @@
-from trytond.model import ModelSQL, ModelView
+from trytond.model import ModelSQL, ModelView, fields
 
 
 __all__ = [
     'Genre',
     'Editor',
+    'EditorGenreRelation',
     'Author',
     'Book',
-    'Exemplary'
+    'Exemplary',
     ]
 
 
@@ -19,17 +20,44 @@ class Editor(ModelSQL, ModelView):
     'Editor'
     __name__ = 'library.editor'
 
-   
+    genres = fields.Many2Many('library.editor-library.genre', 'editor',
+        'genre', 'Genres')
+
+
+class EditorGenreRelation(ModelSQL):
+    'Editor - Genre relation'
+    __name__ = 'library.editor-library.genre'
+
+    editor = fields.Many2One('library.editor', 'Editor', required=True,
+        ondelete='CASCADE')
+    genre = fields.Many2One('library.genre', 'Genre', required=True,
+        ondelete='RESTRICT')
+
+
 class Author(ModelSQL, ModelView):
     'Author'
     __name__ = 'library.author'
 
-   
+    books = fields.One2Many('library.book', 'author', 'Books')
+
+
 class Book(ModelSQL, ModelView):
     'Book'
     __name__ = 'library.book'
+
+    author = fields.Many2One('library.author', 'Author', required=True,
+        ondelete='CASCADE')
+    exemplaries = fields.One2Many('library.book.exemplary', 'book',
+        'Exemplaries')
+    genre = fields.Many2One('library.genre', 'Genre', ondelete='RESTRICT',
+        required=False)
+    editor = fields.Many2One('library.editor', 'Editor', ondelete='RESTRICT',
+        required=True)
 
 
 class Exemplary(ModelSQL, ModelView):
     'Exemplary'
     __name__ = 'library.book.exemplary'
+
+    book = fields.Many2One('library.book', 'Book', ondelete='CASCADE',
+        required=True)
