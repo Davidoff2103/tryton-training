8b25320898a2d6602f074018b7b3efc45f75e0dd
Davidoff2103 <davidoff.d777@gmail.com>
2021-06-14 11:22:07 +0200

    Exercici final

---
+++ tryton-training/modules/library_borrow/__init__.py
@@ -0,0 +1,19 @@
+from trytond.pool import Pool
+
+from . import library
+from . import wizard
+
+def register():
+    Pool.register(
+        library.User,
+        library.Checkout,
+        library.Exemplary,
+        library.Book,
+        wizard.BorrowSelectBooks,
+        wizard.ReturnSelectCheckouts,
+        module='library_borrow', type_='model')
+
+    Pool.register(
+        wizard.Borrow,
+        wizard.Return,
+        module='library_borrow', type_='wizard')
